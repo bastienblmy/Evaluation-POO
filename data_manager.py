6ae9a977040dfbd746f4f684b6f7fc46211a327a
Bastien Barthélémy <email@exemple.com>
2026-02-13 14:42:47 +0100

    Barthelemy - 07-data-manager-chargement-json

--- data_manager.py
+++ data_manager.py
@@ -1,1 +1,15 @@
-# Chargement / sauvegarde JSON
+import json
+from models.client import Client
+from models.vehicule import Vehicule
+
+
+def charger_clients():
+    with open("clients.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return [Client.from_dict(d) for d in data]
+
+
+def charger_vehicules():
+    with open("vehicules.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return [Vehicule.from_dict(d) for d in data]
--- main.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Projet location véhicules - initialisation")
-
-if __name__ == "__main__":
-    main()

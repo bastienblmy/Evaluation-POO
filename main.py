5e1010f4c562beb20aff7f20d410f832ca05de76
Bastien Barthélémy <email@exemple.com>
2026-02-13 14:51:11 +0100

    Barthelemy - 09-ui-affichage-clients-vehicules

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import ui
+import data_manager
 
 
 def main():
+    clients = data_manager.charger_clients()
+    vehicules = data_manager.charger_vehicules()
+
     while True:
         ui.afficher_menu()
         choix = ui.demander_choix()
 
-        if choix == "7":
+        if choix == "1":
+            ui.afficher_clients(clients)
+
+        elif choix == "2":
+            ui.afficher_vehicules(vehicules)
+
+        elif choix == "7":
             break
+
         else:
             print("Choix invalide.")
 
--- ui.py
+++ ui.py
@@ -2,9 +2,29 @@ def afficher_menu():
     print("============================================================")
     print("SYSTÈME DE LOCATION DE VÉHICULES")
     print("============================================================")
+    print("1. Afficher les clients")
+    print("2. Afficher les véhicules")
     print("7. Quitter")
     print("============================================================")
 
 
 def demander_choix():
     return input("Votre choix : ").strip()
+
+
+def afficher_clients(clients):
+    print("============================================================")
+    print("LISTE DES CLIENTS")
+    print("============================================================")
+    for c in clients:
+        print(c)
+    print("============================================================")
+
+
+def afficher_vehicules(vehicules):
+    print("============================================================")
+    print("LISTE DES VÉHICULES")
+    print("============================================================")
+    for v in vehicules:
+        print(v)
+    print("============================================================")

bf08d5db013898d3fa4e2ecaca0896857e0a59eb
Bastien Barthélémy <email@exemple.com>
2026-02-13 14:16:45 +0100

    Barthelemy - 04-serialisation-client-vehicule

--- models/clients.py
+++ models/clients.py
@@ -9,3 +9,24 @@ class Client:
 
     def __str__(self):
         return f"{self.id_client} - {self.prenom} {self.nom}"
+    
+    def to_dict(self):
+        return {
+            "id_client": self.id_client,
+            "nom": self.nom,
+            "prenom": self.prenom,
+            "mail": self.mail,
+            "telephone": self.telephone,
+            "adresse": self.adresse,
+        }
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(
+            d.get("id_client", ""),
+            d.get("nom", ""),
+            d.get("prenom", ""),
+            d.get("mail", ""),
+            d.get("telephone", ""),
+            d.get("adresse", ""),
+        )
--- models/vehicules.py
+++ models/vehicules.py
@@ -9,3 +9,24 @@ class Vehicule:
 
     def __str__(self):
         return f"{self.id_vehicule} - {self.marque} {self.modele} ({self.cylindree})"
+    
+    def to_dict(self):
+        return {
+            "id_vehicule": self.id_vehicule,
+            "marque": self.marque,
+            "modele": self.modele,
+            "cylindree": self.cylindree,
+            "kilometrage_actuel": self.kilometrage_actuel,
+            "date_mise_en_circulation": self.date_mise_en_circulation,
+        }
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(
+            d.get("id_vehicule", ""),
+            d.get("marque", ""),
+            d.get("modele", ""),
+            d.get("cylindree", 0),
+            d.get("kilometrage_actuel", 0),
+            d.get("date_mise_en_circulation", ""),
+        )

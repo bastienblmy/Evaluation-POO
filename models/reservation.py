8dcd4350ebbb7846b169368f0254c857de48a491
Bastien Barthélémy <email@exemple.com>
2026-02-13 14:58:05 +0100

    Barthelemy - 11-reservation-calcul-cout-estime

--- models/reservation.py
+++ models/reservation.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Reservation:
     def __init__(self, id_reservation, id_client, id_vehicule, date_depart, date_retour, forfait_km, cout_journalier, prix_km_supp, cout_estime):
         self.id_reservation = id_reservation
@@ -8,4 +10,13 @@ class Reservation:
         self.forfait_km = forfait_km
         self.cout_journalier = cout_journalier
         self.prix_km_supp = prix_km_supp
-        self.cout_estime = cout_estime
+        self.cout_estime = self._calculer_cout_estime()
+
+    
+    def _calculer_cout_estime(self):
+        d1 = date.fromisoformat(self.date_depart)
+        d2 = date.fromisoformat(self.date_retour)
+        nb_jours = (d2 - d1).days
+        if nb_jours < 1:
+            nb_jours = 1
+        return nb_jours * self.cout_journalier
